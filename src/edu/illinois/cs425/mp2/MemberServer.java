1594d960b45164487c8a36b895baf369be92a886
srikanth <srikanth@srikanth-Lenovo-Z580>
2012-09-29 03:07:36 -0500

    Adding command prompt to the server which can be used for testing.

--- src/edu/illinois/cs425/mp2/MemberServer.java
+++ src/edu/illinois/cs425/mp2/MemberServer.java
@@ -1,9 +1,10 @@
 package edu.illinois.cs425.mp2;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
-import java.net.MulticastSocket;
 import java.net.SocketException;
 import java.util.Arrays;
 import java.util.LinkedList;
@@ -21,9 +22,9 @@ public class MemberServer {
 	private MemberNode node;
 	private volatile MemberNode neighborNode;
 	private volatile Timer lastReceivedHeartBeat;
-	private volatile boolean sendHeartBeat=false;
+	private volatile boolean sendHeartBeat = false;
 	private volatile List<MemberNode> globalList;
-	
+
 	public MemberNode getNeighborNode() {
 		return neighborNode;
 	}
@@ -31,7 +32,7 @@ public class MemberServer {
 	public void setNeighborNode(MemberNode neighborNode) {
 		this.neighborNode = neighborNode;
 	}
-    
+
 	private MemberServer() {
 		// initially doesn't have any neighbors
 		this.neighborNode = null;
@@ -39,7 +40,7 @@ public class MemberServer {
 		this.sendHeartBeat = false;
 	}
 
-    public Timer getLastReceivedHeartBeat() {
+	public Timer getLastReceivedHeartBeat() {
 		return lastReceivedHeartBeat;
 	}
 
@@ -55,12 +56,12 @@ public class MemberServer {
 		this.sendHeartBeat = sendHeartBeat;
 	}
 
-
-	public static MemberServer start(String hostName, int hostPort) throws SocketException {
-        MemberServer server = new MemberServer();
-        server.setNode(MemberNode.start(hostName, hostPort));
-        server.setNeighborNode(server.getNode());
-        return server;
+	public static MemberServer start(String hostName, int hostPort)
+			throws SocketException {
+		MemberServer server = new MemberServer();
+		server.setNode(MemberNode.start(hostName, hostPort));
+		server.setNeighborNode(server.getNode());
+		return server;
 	}
 
 	public void setNode(MemberNode node) {
@@ -73,41 +74,71 @@ public class MemberServer {
 	}
 
 	public DatagramSocket getSocket() {
-       return node.getSocket();
+		return node.getSocket();
 	}
-	public static void main(String[] args)  {
 
-//		if(args.length < 2) {
-//			System.out.println("Usage: java <portnumber>");
-//            return;
-//		}
+	public static void main(String[] args) {
+
+		// if(args.length < 2) {
+		// System.out.println("Usage: java <portnumber>");
+		// return;
+		// }
 		MemberServer server = null;
 		MemberServer neighbor = null;
-		
+
 		boolean listening = true;
 		try {
 			server = MemberServer.start("localhost", Integer.parseInt("5070"));
-			server.sendHeartBeat=true;
-		    neighbor = MemberServer.start("localHost",5090 );
-		     byte[] buf = new Message(MessageTypes.JOIN).toBytes();
-		     DatagramPacket packet = new DatagramPacket(buf,buf.length,server.node.getHostAddress(),server.node.getHostPort());
-		     neighbor.getSocket().send(packet);
+			server.sendHeartBeat = true;
+			neighbor = MemberServer.start("localHost", 5090);
+			byte[] buf = new Message(MessageTypes.JOIN).toBytes();
+			DatagramPacket packet = new DatagramPacket(buf, buf.length,
+					server.node.getHostAddress(), server.node.getHostPort());
+			neighbor.getSocket().send(packet);
 		} catch (SocketException e) {
 			System.out.println("Error: Unable to open socket");
 			System.exit(-1);
 		} catch (IOException e) {
 			System.out.println("Could not	 listen on port.");
 			System.exit(-1);
-		}
-		catch(Exception e) {
+		} catch (Exception e) {
 			System.out.println("Byte Construction failed");
 			System.exit(-1);
 		}
-        //starting heartbeat thread
-        new HeartBeatServiceThread(server).start();
-        
-        while(listening)
-        { new Processor(server).run();
-         
+		// starting heartbeat thread
+		new HeartBeatServiceThread(server).start();
+		new ProcessorThread(server).start();
+
+		try {
+			DatagramSocket socket;
+			socket = new DatagramSocket();
+			String inputLine;
+			BufferedReader in = new BufferedReader(new InputStreamReader(
+					System.in));
+			System.out.print("[Please Enter Command]$ ");
+			while ((inputLine = in.readLine()) != null) {
+				if (inputLine.startsWith("join")) {
+					byte[] buf = new byte[256];
+					buf = inputLine.getBytes();
+					// InetAddress address = InetAddress.getByName(args[0]);
+					DatagramPacket packet = new DatagramPacket(buf, buf.length);
+					socket.send(packet);
+				} else if (inputLine.startsWith("leave")) {
+
+				} else if (inputLine.equals("help")) {
+					System.out
+							.println("Usage: [join|leave] <hostname:hostport>");
+				} else if (inputLine.equals("exit")) {
+					System.exit(0);
+				} else {
+					System.out.println("use help for possible options");
+				}
+				System.out.print("[Please Enter Command]$ ");
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
 	}
-}}
+}
---
+++ src/edu/illinois/cs425/mp2/ProcessorThread.java
@@ -0,0 +1,137 @@
+package edu.illinois.cs425.mp2;
+
+import java.io.ByteArrayInputStream;
+import java.io.ObjectInputStream;
+import java.net.DatagramPacket;
+import java.net.InetAddress;
+import java.util.TimerTask;
+
+public class ProcessorThread extends Thread {
+	private static MemberServer server;
+	TimerTask task ;
+
+
+	public ProcessorThread(MemberServer server) {
+         ProcessorThread.server=server;
+         initializeTimerTask();
+         }
+
+
+
+
+    @Override
+	public void run() {
+
+		DatagramPacket packet;
+		Message message;
+		InetAddress senderAddress;
+		int port;
+		byte receiveMessage[] = new byte[Message.MAX_MESSAGE_LENGTH];
+
+		try
+		{
+			packet=new DatagramPacket(receiveMessage,receiveMessage.length);
+			server.getNode().getSocket().receive(packet);
+			senderAddress = packet.getAddress();
+			port = packet.getPort();
+			ByteArrayInputStream bis = new ByteArrayInputStream(packet.getData());
+			ObjectInputStream in = null;
+		    in = new ObjectInputStream(bis);
+		    message = (Message)in.readObject();
+			if(message.getMessageType().equals(MessageTypes.JOIN))
+			{
+				System.out.println("Join message receieved");
+				new ServiceThread(senderAddress,port,message)
+			  	{  @Override
+				public void run()
+			  	    {
+			  		try {
+
+						serviceJoinRequest(this.getAddr(),this.getPort(),this.getMessage());
+					} catch (Exception e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+			  	    }
+			  	}.start();
+			}
+			else if(message.getMessageType().equals(MessageTypes.HEART_BEAT))
+			{
+		        //System.out.println("heartbeat receieved");
+				updateTimer();
+		    }
+			else if(message.getMessageType().equals(MessageTypes.LEAVE))
+			{
+				new Thread()
+				{
+					@Override
+					public void run()
+					{
+						serviceLeaveRequest();
+					}
+				}.start();
+			}
+			else if (message.getMessageType().equals(MessageTypes.JOIN_ACK))
+			{
+				System.out.println("Join ack receieved");
+				onJoinAck(senderAddress,port,message);
+			}
+
+		}catch(Exception e)
+		{
+
+
+		}
+
+
+
+	}
+
+	public void updateTimer()
+	{
+		ProcessorThread.server.getLastReceivedHeartBeat().cancel();
+		ProcessorThread.server.getLastReceivedHeartBeat().schedule(this.task,2*1000);
+	}
+
+	public void serviceJoinRequest(InetAddress addr, int port, Message message) throws Exception
+	{
+	    System.out.println("Servicing Join request");
+		server.setNeighborNode(new MemberNode(addr,port));
+	    byte [] buf = new Message(MessageTypes.JOIN_ACK).toBytes();
+	    DatagramPacket packet = new DatagramPacket(buf,buf.length,addr,port);
+	    ProcessorThread.server.getSocket().send(packet);
+	    //multicast to all servers too
+     }
+
+	public void onJoinAck(InetAddress addr,int port,Message message)
+	{
+		System.out.println("Join Acknowledging");
+		ProcessorThread.server.setNeighborNode(new MemberNode(addr,port));
+		ProcessorThread.server.setSendHeartBeat(true);
+	}
+	public void serviceLeaveRequest()
+	{
+
+	}
+
+	public void processFailure()
+	{
+		System.out.println("failure Detected");
+		//TODO update table and multicast
+	}
+
+	public void initializeTimerTask()
+	{
+		this.task = new TimerTask(){
+			@Override
+			public void run()
+			{
+				System.out.println("failure Detected");
+				processFailure();
+			}
+		};
+
+	}
+}
+
+
